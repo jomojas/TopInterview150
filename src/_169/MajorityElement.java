754dbd5addf0d3711db6272a765dbcd51e2e2b2b
Jomo <jomojassen@gmail.com>
2025-04-21 09:08:55 +0800

    _169 Find Majority Element and return

---
+++ src/_169/MajorityElement.java
@@ -0,0 +1,35 @@
+package _169;
+
+public class MajorityElement {
+    public static void main(String[] args) {
+        int[] nums = {2, 2, 1, 1, 1, 2, 2};
+        Solution solution = new Solution();
+        int majorityElement = solution.majorityElement(nums);
+        System.out.println("Majority Element: " + majorityElement);
+    }
+}
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+        for (int elem : nums) {
+            /* My method */
+            // if (elem == candidate) {
+            //     count++;
+            // } else {
+            //     if (count == 0) {
+            //         candidate = elem;
+            //     } else {
+            //         count--;
+            //     }
+            // }
+            /* Solution */
+            if (count == 0) {
+                candidate = elem;
+            }
+            count += (elem == candidate) ? 1 : -1;
+        }
+         return candidate;
+    }
+}
